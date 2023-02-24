23cf6f1174e5157df0940a7f2ae2c2aba6cb2b1d
Anna <annamtoman@gmail.com>
2023-02-24 14:27:05 -0600

    adds data

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,3 +1,20 @@
 require "http"
 system "clear"
-puts "Welcome to the dictionary!"
+
+puts "Welcome to the dictionary! Type a word of your choosing to get a definition."
+
+word = gets.chomp 
+
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=APIKEYGOESHERE")
+response_2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=APIKEYGOESHERE
+")
+
+definition = response.parse(:json)
+pronunciation = response_2.parse(:json)
+
+# pp "Your word means: #{definition[0]["text"]}. The top example of a #{definition[0]["text"]} is: #{definition[0]["exampleUses"]}."
+
+pp pronunciation
+
+
+
